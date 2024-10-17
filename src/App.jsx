8347698ff47b13780282df11250244e953e9fe19
Ricardo Martins dos Reis <ricardomreis22@hotmail.com>
2024-10-17 03:07:26 +0100

    update(newQuizzical)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,118 @@
-import React, { useState, useEffect } from 'react'
-
-import Start from './Start'
-import Question from './Question'
+import React, { useState, useEffect } from "react";
+import Question from "./Question";
+import Start from "./Start";
+import { decode } from "html-entities";
 
 export default function App() {
+  const [questionsComponent, setQuestionsComponent] = useState(false);
+  const [quizzicalData, setQuizzicalData] = useState([]);
+  const [button, setButton] = useState(false);
+  const [count, setCount] = useState(0);
 
-    const [questionsComponent, setQuestionsComponent] = useState(false)
-    const [quizzicalData, setQuizzicalData ] = useState([])
-
-    console.log(quizzicalData)
-    console.log(questionsComponent)
-    useEffect(() =>{
-        fetch("https://opentdb.com/api.php?amount=5")
-        .then(res => res.json())
-        .then(data => setQuizzicalData(data.results))
-        console.log("render")
-    }, [])
-
-    // Toggle between starting page and questions page
-    function changePage() {
-        console.log("1")
-        setQuestionsComponent(prev => !prev)    
+  // Helper function to shuffle an array
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
+    return array;
+  }
 
-    return (
-        <div>
-            {/* toggle between starting page and question page */}
-            { !questionsComponent && < Start startQuiz={changePage}/>}
-            { questionsComponent && < Question startPage={changePage} quizzicalData={quizzicalData}/>}
-            {/* <pre>{JSON.stringify(quizicalData, null, 2)}</pre> */}
-        </div>
-    )
+  // Helper function to decode the answers array
+  function decodeArr(array) {
+    array.map((item) => {
+      item = decode(item, { level: "all" });
+    });
+  }
 
-} 
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://opentdb.com/api.php?amount=5")
+        .then((res) => res.json())
+        .then((data) => {
+          const dataArr = data.results?.map((questionObj) => {
+            return {
+              ...questionObj,
+              question: decode(questionObj.question, { level: "all" }),
+              answers: decode(
+                shuffle([
+                  ...questionObj.incorrect_answers,
+                  questionObj.correct_answer,
+                ])
+              ),
+              selectedAnswer: "",
+              isCorrect: "",
+            };
+          });
+          setQuizzicalData(dataArr);
+        });
+    };
+    if (quizzicalData.length === 0) {
+      fetchData();
+    }
+  }, [questionsComponent]);
+
+  // Toggle between starting page and questions page
+  function changePage() {
+    setQuestionsComponent((prev) => !prev);
+    setButton(false);
+  }
+
+  // When select an answer that answer goes to selectedAnswers property on that answer Question Object
+  function select(event, answer, questionObjet) {
+    setQuizzicalData((prevQuizzical) => {
+      return prevQuizzical.map((questionObj) => {
+        if (questionObj.question === questionObjet.question) {
+          return { ...questionObj, selectedAnswer: `${answer}` };
+        } else {
+          return { ...questionObj };
+        }
+      });
+    });
+  }
+
+  function checkAnswers() {
+    setButton(true);
+    setQuizzicalData((prev) => {
+      setCount(0);
+      return prev.map((questionObj) => {
+        if (questionObj.selectedAnswer) {
+          if (questionObj.correct_answer === questionObj.selectedAnswer) {
+            setCount((prev) => prev + 1);
+            return { ...questionObj, isCorrect: "correct" };
+          } else {
+            return { ...questionObj, isCorrect: "incorrect" };
+          }
+        } else {
+          return { ...questionObj };
+        }
+      });
+    });
+  }
+
+  return (
+    <div>
+      {/* toggle between starting page and question page */}
+      {!questionsComponent && <Start startQuiz={changePage} />}
+      {questionsComponent && (
+        <Question
+          startPage={changePage}
+          quizzicalData={quizzicalData}
+          select={select}
+          checkAnswers={checkAnswers}
+          button={button}
+          count={count}
+        />
+      )}
+    </div>
+  );
+}
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,107 +1,100 @@
-import React, { useState, useEffect } from 'react'
-import {decode} from 'html-entities';
+import React, { useState, useEffect } from "react";
 
+export default function Question(props) {
+  const questionsArr = props.quizzicalData?.map((questionObj) => {
+    return questionObj;
+  });
 
-export default function Question (props) {
-
-    let data = props.quizzicalData.map((item, index) => {
-        // we get the incorrect answers array and insert the correct answer into that array using splice
-        console.log("item",item.incorrect_answers)
-        let answers = item.incorrect_answers
-        answers.splice(1, 0, item.correct_answer).sort()
-        // than we sort the array randomly
-        // return the newData array of objects, with an objet for each question we have
-        return {
-            id:index,
-            key:index,
-            question: decode(item.question),
-            answers: answers
-        }     
-    })  
-    
-    console.log("data", data)
-
-    // newData state used to get the questions and answers we gonna rend
-    const [newData, setNewData] = useState([])
-    
-    
-    // everytime the data changes we use this effect to setNewData with the new values from data
-    useEffect(() => {
-        setNewData(() => {
-            // for every item in data array that has the 5 objects with key:values
-            return data.map((item, index) => {
-                // we get the incorrect answers array and insert the correct answer into that array using splice
-                let answers = item.incorrect_answers
-                answers.splice(1, 0, item.correct_answer).sort()
-                // than we sort the array randomly
-                // return the newData array of objects, with an objet for each question we have
-                return {
-                    id:index,
-                    key:index,
-                    question: decode(item.question),
-                    answers: answers
-                }        
-            })
-        })
-    }, [data])
-
-
-    // create a function to select on of the answer
-    function select (event) {
-        let selectAnswer = []
-        selectAnswer.push(event.target.value)
-        console.log(selectAnswer)
-    }
-
-    function checkAnswers () {
-        // get the correct answer from every question
-        data.map( (item) => {
-            console.log(item.correct_answer) 
-        })
-    }
-
-    
-
-    return (
-        <div>
-            <h3 className="question"></h3>
-             <div>
-                {/* {map through every question we get from the API} */}
-                {newData.map((questionObj) => (
-                    <div 
-                    key={questionObj.id}
-                    id={questionObj.id}
-                    >
-                        <p>{questionObj.question}</p>   
-                        <form>
-                            {/* map through every possible answer */}
-                            {questionObj.answers.map((answer) => (
-                                <div>
-                                    <input 
-                                        key={answer}
-                                        type="radio"
-                                        id={answer}
-                                        name="selectedAnswer"
-                                        value={answer}
-                                        onClick={select}
-                                    />
-                                    <label htmlFor={answer}>{answer}</label>
-                                    <br />
-                                </div>
-                            ))}
-
-                        </form>
-
-                    </div>
-                ))}
+  return (
+    <div className="container questions-page">
+      <svg
+        className="question-SVG yellow-SVG"
+        xmlns="http://www.w3.org/2000/svg"
+        width="158"
+        height="141"
+        viewBox="0 0 158 141"
+        fill="none"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M63.4095 81.3947C35.1213 50.8508 -2.68211 21.7816 1.17274 -19.6933C5.43941 -65.599 39.854 -105.359 82.4191 -123.133C122.797 -139.994 170.035 -130.256 205.822 -105.149C235.947 -84.0141 236.823 -43.8756 246.141 -8.27104C256.17 30.0508 282.521 70.8106 260.501 103.779C237.538 138.159 188.991 143.432 147.931 138.768C112.318 134.723 87.7505 107.677 63.4095 81.3947Z"
+          fill="#FFFAD1"
+        />
+      </svg>
+      <div className="questions-container">
+        {props.quizzicalData?.map((questionObjet, index) => {
+          return (
+            <div key={index} id={index} className="question-obj">
+              <p className="question">{questionObjet.question}</p>
+              <div className="answers-container">
+                <form>
+                  {questionObjet?.answers?.map((answer, index) => {
+                    return (
+                      <div key={index} className="radioButton">
+                        {!props.button && (
+                          <input
+                            type="radio"
+                            onClick={(event) =>
+                              props.select(event, answer, questionObjet)
+                            }
+                            id={index}
+                            name="selected"
+                          ></input>
+                        )}
+                        <label
+                          className={`radioButton-label ${
+                            answer === questionObjet.selectedAnswer
+                              ? questionObjet.isCorrect
+                              : ""
+                          } `}
+                          htmlFor={answer}
+                          id={index}
+                        >
+                          {answer}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </form>
+              </div>
+              <hr></hr>
             </div>
-            <br></br>            
-            <div className="div">
-                
-            </div>
-            <button onClick={checkAnswers}>Check Answers</button>                    
-            <button onClick={props.startPage}>Play again</button>
-        </div>
-    )
-
+          );
+        })}
+      </div>
+      <div className="button-container">
+        {props.button && (
+          <p>
+            You scored {props.count}/{props.quizzicalData.length} correct
+            answers
+          </p>
+        )}
+        {props.button ? (
+          <button className="btn-default" onClick={props.startPage}>
+            Play again
+          </button>
+        ) : (
+          <button className="btn-default" onClick={props.checkAnswers}>
+            Check Answers
+          </button>
+        )}
+      </div>
+      <svg
+        className="question-SVG blue-SVG"
+        xmlns="http://www.w3.org/2000/svg"
+        width="148"
+        height="118"
+        viewBox="0 0 148 118"
+        fill="none"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M-5.55191 4.90596C35.9614 1.77498 82.2425 -9.72149 112.306 19.1094C145.581 51.0203 155.282 102.703 142.701 147.081C130.767 189.18 93.7448 220.092 51.8208 232.476C16.5281 242.902 -15.4332 218.605 -49.1007 203.738C-85.3375 187.737 -133.641 182.993 -145.741 145.239C-158.358 105.868 -132.269 64.5881 -103.064 35.3528C-77.7328 9.99541 -41.2727 7.60006 -5.55191 4.90596Z"
+          fill="#DEEBF8"
+        />
+      </svg>
+    </div>
+  );
 }
